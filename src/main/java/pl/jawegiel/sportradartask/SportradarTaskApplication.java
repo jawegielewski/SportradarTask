07feedc28df1427e0ba093fe507ce3713a5892fa
jawegielewski <jawegielewski@gmail.com>
2026-03-22 14:44:02 +0100

    Freshly created project in IntelliJ Idea

---
+++ src/main/java/pl/jawegiel/sportradartask/SportradarTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.jawegiel.sportradartask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportradarTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportradarTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jawegiel/sportradartask/SportradarTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jawegiel.sportradartask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportradarTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
